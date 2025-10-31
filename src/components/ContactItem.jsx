68849c5570e5d8b629467fd8e93f5d112ceae868
Pantea Saei <pantisa2020@gmail.com>
2025-10-31 09:25:04 -0700

    create single contact item component with styles

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -3,10 +3,17 @@ import styles from './ContactItem.module.css'
 import { FaTrashCan,FaPhoneVolume } from "react-icons/fa6";
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { RiEdit2Fill } from "react-icons/ri";
-function ContactItem({data:{id,name,lastName,email,phone},handleDelete,editHandler}) {
+function ContactItem({data:{id,name,lastName,email,phone},handleDelete,editHandler,toggleHandler,selectId}) {
   
   return (
     <li className={styles.item}>
+      <div
+      className={styles.section}>
+        <input type='checkbox'
+        checked={selectId.includes(id)}
+        onChange={()=>toggleHandler(id)}
+        className={styles.checkbox}/>
+      </div>
               <p>
                 {name}
                 {lastName}
